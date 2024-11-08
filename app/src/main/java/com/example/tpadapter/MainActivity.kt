eb2e2cf436e2497612fb609f9ea2edbcf641c56f
AnassLoriga <lorigaanas1@gmail.com>
2024-11-08 11:44:55 +0100

    changes done

--- app/src/main/java/com/example/tpadapter/MainActivity.kt
+++ app/src/main/java/com/example/tpadapter/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.tpadapter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val lv: ListView = findViewById(R.id.lV)
+        val languages = listOf("Java", "C#", "Python", "Kotlin", "Swift")
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
+
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, languages)
+//        lv.adapter = adapter
+//        lv.choiceMode = ListView.CHOICE_MODE_SINGLE
+
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, languages)
+        lv.adapter = adapter
+        lv.choiceMode = ListView.CHOICE_MODE_MULTIPLE
     }
 }
+
